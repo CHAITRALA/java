5d2219c9a4205e741bb2a53e47184fb644499085
CHAITRALA <chaitraasok@gmail.com>
2023-02-14 12:59:54 +0530

    message

---
+++ src/main/java/obsquara/Addition.java
@@ -0,0 +1,20 @@
+package obsquara;
+
+
+
+public class Addition {
+
+	public static int sum(int a,int b)
+	{ 
+	int sum=a+b;
+	System.out.println("sum="+sum);
+	return sum ;
+		}
+		
+		
+	public static void main(String args[])
+	{
+	Addition.sum(10,50);
+
+	}		
+}
