22007f61f0bffbbceac08d8d191613264e101a42
CHAITRALA <chaitraasok@gmail.com>
2023-03-10 14:55:32 +0530

    pushing

---
+++ src/main/java/obsquara/HelloWorld123.java
@@ -0,0 +1,19 @@
+package obsquara;
+
+public class HelloWorld123 {
+
+	public static int sum(int a,int b)
+	{ 
+	int sum=a+b;
+	//System.out.println("sum="+sum);
+			System.out.println("hii");
+	return sum ;
+		}
+		
+		
+	public static void main(String args[])
+	{
+	Addition.sum(10,50);
+	System.out.println("hii");
+	System.out.println("hello");
+}}
